9b4dbfdf7d1ec78436d81026233c7962320502b5
Dream K <dreamk@ZoneBook.local>
2023-05-19 16:54:10 -0400

    fix database

--- database/db.js
+++ database/db.js
@@ -9,9 +9,9 @@ console.log("Opening database connection");
 
 // This is our entry point, we instantiate the Sequelize instance accordingly;
 const db = new Sequelize(dbName, dbUser, dbPwd, {
-  host: "localhost",
+  host: "containers-us-west-45.railway.app",
   dialect: "postgres",
-  port: 5432,
+  port: 5776,
 });
 // Export our instance of Sequelize, which will be modified with models;
 module.exports = db;
--- database/utils/configDB.js
+++ database/utils/configDB.js
@@ -1,9 +1,15 @@
-const dbName = 'final-backend';
+const dbName = 'railway';
 const dbUser = 'postgres';
-const dbPwd = '';
+const dbPwd = 'jL6IrL9u957e5Do88OTA';
+const dbHost = 'containers-us-west-45.railway.app';
+// const url = '';
+// const dialect = '';
 
 module.exports = {
   dbName,
   dbUser,
   dbPwd,
+  dbHost,
+//   url,
+//   dialect,
 };
